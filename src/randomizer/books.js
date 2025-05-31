cf961751581b1408d0c3424f46e989f95bca4def
nayem-uddin <mnubpial@gmail.com>
2025-05-31 19:36:32 +0600

    adjust to some extent
    -remove unnecessary code fragments and comments

--- src/randomizer/books.js
+++ src/randomizer/books.js
@@ -4,102 +4,76 @@ import { review_ru, reviews_bn, reviews_en } from "./reviews.js";
 
 export default function handleQuery(query, maxNum) {
   const { lang, seed, likes, reviews } = query;
+  const likesNumerized = Number(likes);
+  const reviewsNumerized = Number(reviews);
   if (lang === "bn") {
     const customFaker = new Faker({ locale: [bn] });
-    customFaker.seed(Number(seed) + 1);
-    return booksGen(customFaker, maxNum, reviews_bn, likes, reviews);
+    customFaker.seed(Number(seed) + likesNumerized + reviewsNumerized + 1);
+    return booksGen(
+      customFaker,
+      maxNum,
+      reviews_bn,
+      likesNumerized,
+      reviewsNumerized
+    );
   } else if (lang === "en") {
     const customFaker = new Faker({ locale: [en] });
-    customFaker.seed(Number(seed));
-    return booksGen(customFaker, maxNum, reviews_en, likes, reviews);
+    customFaker.seed(Number(seed) + likesNumerized + reviewsNumerized);
+    return booksGen(
+      customFaker,
+      maxNum,
+      reviews_en,
+      likesNumerized,
+      reviewsNumerized
+    );
   } else if (lang === "ru") {
     const customFaker = new Faker({ locale: [ru] });
-    customFaker.seed(Number(seed) - 1);
-    return booksGen(customFaker, maxNum, review_ru, likes, reviews);
+    customFaker.seed(Number(seed) + likesNumerized + reviewsNumerized - 1);
+    return booksGen(
+      customFaker,
+      maxNum,
+      review_ru,
+      likesNumerized,
+      reviewsNumerized
+    );
   }
 }
 
 function booksGen(customFaker, maxNum, reviewsList, likes, reviews) {
   const books = [];
-  const iters = maxNum / 10;
+  const sizePerSegment = 10;
+  const iters = maxNum / sizePerSegment;
   for (let i = 0; i < iters; i++) {
     const tenBooks = perTen(customFaker, likes, reviews, reviewsList);
     books.push(...tenBooks);
   }
+
   return books;
 }
 
 function perTen(faker, totalLikes, totalReviews, reviewsList) {
   const arrayOfTen = [];
-  const numerizedReviews = Number(totalReviews);
-  const numerizedLikes = Number(totalLikes);
-  const highestReviews = Math.ceil(numerizedReviews);
-  const lowestReviews = Math.floor(numerizedReviews);
-  const highestLikes = Math.ceil(numerizedLikes);
-  const lowestLikes = Math.floor(numerizedLikes);
-  const expectedHighestLikesCount = (numerizedLikes - lowestLikes) * 10;
-  const expectedHighestReviewsCount = (numerizedReviews - lowestReviews) * 10;
+  const highestReviews = Math.ceil(totalReviews);
+  const lowestReviews = Math.floor(totalReviews);
+  const highestLikes = Math.ceil(totalLikes);
+  const lowestLikes = Math.floor(totalLikes);
+  const expectedHighestLikesCount = (totalLikes - lowestLikes) * 10;
+  const expectedHighestReviewsCount = (totalReviews - lowestReviews) * 10;
   let highestLikesCount = 0;
   let highestReviewsCount = 0;
-  // let attempts = 0;
-  // const MAX_ATTEMPTS = 100;
-  // const candidates = [];
   while (arrayOfTen.length < 10) {
     const isExpectedLikesReached =
       highestLikesCount === expectedHighestLikesCount;
     const likes = isExpectedLikesReached ? lowestLikes : highestLikes;
-    highestLikesCount += Number(!isExpectedLikesReached);
+    highestLikesCount += isExpectedLikesReached ? 0 : 1;
     const isExpectedReviewsReached =
       highestReviewsCount === expectedHighestReviewsCount;
     const reviews = isExpectedReviewsReached ? lowestReviews : highestReviews;
-    highestReviewsCount += Number(!isExpectedReviewsReached);
+    highestReviewsCount += isExpectedReviewsReached ? 0 : 1;
     const book = createBook(faker, reviewsList, likes, reviews);
     arrayOfTen.push(book);
   }
-
-  // while (arrayOfTen.length < 10 && attempts < MAX_ATTEMPTS) {
-  //   attempts++;
-  //   const book = createBook(faker, reviewsList, totalLikes, totalReviews);
-  //   const { likes, reviews } = book;
-  //   const NoOfReviews = reviews.length;
-  //   const inLikeRange = likes >= lowestLikes && likes <= highestLikes;
-  //   const inReviewRange =
-  //     NoOfReviews >= lowestReviews && NoOfReviews <= highestReviews;
-  //   if (inLikeRange && inReviewRange) {
-  //     let added = false;
-  //     if (
-  //       likes === highestLikes &&
-  //       highestLikesCount < expectedHighestLikesCount
-  //     ) {
-  //       highestLikesCount++;
-  //       arrayOfTen.push(book);
-  //       added = true;
-  //     }
-  //     if (
-  //       !added &&
-  //       NoOfReviews === highestReviews &&
-  //       highestReviewsCount < expectedHighestReviewsCount
-  //     ) {
-  //       highestReviewsCount++;
-  //       arrayOfTen.push(book);
-  //       added = true;
-  //     }
-  //     if (!added && arrayOfTen.length < 10) {
-  //       arrayOfTen.push(book);
-  //     }
-  //   } else {
-  //     candidates.push(book);
-  //   }
-  // }
-  // while (arrayOfTen.length < 10 && candidates.length > 0) {
-  //   arrayOfTen.push(candidates.pop());
-  // }
-
-  // if (arrayOfTen.length < 10) {
-  //   console.warn(
-  //     `Only generated ${arrayOfTen.length} books after ${MAX_ATTEMPTS} attempts.`
-  //   );
-  // }
+  faker.helpers.shuffle(arrayOfTen, { inplace: true });
   return arrayOfTen;
 }
 
@@ -133,25 +107,16 @@ function imgLinkgen(title, author, textColor, coverColor) {
 }
 
 function reviewsgen(faker, reviewsList, reviews) {
-  const minReviews = Math.floor(reviews);
-  const maxReviews = Math.ceil(reviews);
-  return faker.helpers.arrayElements(reviewsList, {
-    max: maxReviews,
-    min: minReviews,
-  });
+  return faker.helpers.arrayElements(reviewsList, reviews);
 }
 
-function createBook(customFaker, reviewsList, noofLikes, noofReviews) {
+function createBook(customFaker, reviewsList, likes, noofReviews) {
   const title = customFaker.book.title();
   const author = customFaker.book.author();
   const textColor = customFaker.color.rgb();
   const coverColor = customFaker.color.rgb({ includeAlpha: true });
   const imgLink = imgLinkgen(title, author, textColor, coverColor);
   const reviews = reviewsgen(customFaker, reviewsList, noofReviews);
-  const likes = customFaker.number.int({
-    max: Math.ceil(noofLikes),
-    min: Math.floor(noofLikes),
-  });
   return {
     title: title,
     author: author,
